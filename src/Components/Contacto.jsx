a634ffd7eb96523e5941d6f44935dd6ae14e2c1f
César <80521851+Cesar-ctrl@users.noreply.github.com>
2022-03-06 20:12:00 +0100

    Enviar correo Implementado

--- src/Components/Contacto.jsx
+++ src/Components/Contacto.jsx
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
-const Contacto = () => {
-  return <div>
-      <h1>Contacto</h1>
-  </div>;
+export const Contacto = () => {
+
+  const [toSend, setToSend] = useState({
+
+    from_name: '',
+    message: '',
+    user_email: '',
+
+  });
+  const [sent,setSent] = useState(false)
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_48u8f14', 'template_nq6p8jm', form.current, 'P_42ms28hsNK4rkum')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      setToSend({
+        from_name: '',
+        message: '',
+        user_email: '',
+      })
+  };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+
+  };
+
+  return (
+    <div id="main2" className='contacto form'>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="from_name" value={toSend.from_name} onChange={handleChange}/>
+        <label>Email</label>
+        <input type="email" name="user_email" value={toSend.user_email} onChange={handleChange}/>
+        <label>Message</label>
+        <textarea name="message" value={toSend.message} onChange={handleChange}/>
+        <input type="submit" value="Send" />
+    </form>
+    </div>
+  );
 };
-export default Contacto;
+export default Contacto
