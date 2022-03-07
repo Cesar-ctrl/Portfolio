5e9b237206f329c313612f69fad63f449edc4bcd
César <80521851+Cesar-ctrl@users.noreply.github.com>
2022-03-07 22:11:24 +0100

    Accesibility

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -7,33 +7,32 @@ const Works = () => {
         <div className="work group">
             <a href='https://github.com/Cesar-ctrl/cesarinsta' className="work card card-body">
                 
-                <img src={process.env.PUBLIC_URL + '/img/home.PNG'} alt="home" />{/** process.env.PUBLIC_URL trae la url baseo url pública. */}
+                
                 
                     To increase my skills as a Full-Stack developer, I developed a social network using PHP, Laravel, and
 Javascript. The project is deployed, you can upload images, comment on images, like images, view user
 profiles and delete or edit comments on your own posts.
-                
+                <img src={process.env.PUBLIC_URL + '/img/home.PNG'} alt="home" />{/** process.env.PUBLIC_URL trae la url baseo url pública. */}
                 
             </a>
 
             <a href="https://cesar-ctrl.github.io"className="work card card-body">
                 
-                <img src={process.env.PUBLIC_URL +'/img/Dnd.PNG'} alt="home" />
                 
                     Designed and developed a web guide about Dungeons and Dragons game, using HTML, CSS and
 JavaScript, among other technologies. This project helped me to improve my layout skills, as well as my
 javascript knowledge. It also allowed me to learn how to consume REST web services.
-                
+                <img src={process.env.PUBLIC_URL +'/img/Dnd.PNG'} alt="home"/>
             </a>
 
             
             <a href="https://cesar-ctrl.github.io/proyecto2/"className="work card card-body">
                 
-                <img src={process.env.PUBLIC_URL +'/img/CrudTareas.PNG'} alt="home" />
+                
                 
                     Developed a CRUD of tasks that is connected to a database, everything that is not deleted stays in the database.
 I've done it with JavaScript, React and Bootstrap, it has state hooks.
-                
+                <img src={process.env.PUBLIC_URL +'/img/CrudTareas.PNG'} alt="home" />
             </a>
             
             
